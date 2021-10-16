7693904294a5e0e013a6d51d6ea8bce06d2530fe
Robson Santos <robsonsantos987@outlook.com>
2021-10-15 21:02:25 -0300

    Primeiro commit

---
+++ js/slides.js
@@ -0,0 +1,15 @@
+const imgbx = document.querySelector('.imgbx');
+   const slides = imgbx.getElementsByTagName('img');
+   var i = 0;
+
+   function nextSlide(){
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+  }
+
+   function PrevSlide(){
+    slides[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
+     }
